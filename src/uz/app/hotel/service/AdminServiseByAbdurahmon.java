a9635690915cc20127e145320007be4c947c60a6
abu-1n <baxtiyarvic11@gmail.com>
2024-05-18 15:47:03 +0500

    Admini class

---
+++ src/uz/app/hotel/service/AdminServiseByAbdurahmon.java
@@ -0,0 +1,54 @@
+package uz.app.hotel.service;
+
+public class AdminServiseByAbdurahmon implements AdminService{
+
+    @Override
+    public void service() {
+
+    }
+
+    @Override
+    public void addHotel() {
+
+    }
+
+    @Override
+    public void showHotel() {
+
+    }
+
+    @Override
+    public void showHotels() {
+
+    }
+
+    @Override
+    public void editHotel() {
+
+    }
+
+    @Override
+    public void deleteHotel() {
+
+    }
+
+    @Override
+    public void showUsers() {
+
+    }
+
+    @Override
+    public void showReservationHistory() {
+
+    }
+
+    @Override
+    public void calcelReservation() {
+
+    }
+
+    @Override
+    public void reserveForUser() {
+
+    }
+}
